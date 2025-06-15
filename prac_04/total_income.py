d8361d6b3ad56427b9d6d3c71daddf39cde20cec
22767 <2276733864@qq.com>
2025-06-15 16:27:47 +0800

    finish the main part for the total_income.py

---
+++ prac_04/total_income.py
@@ -0,0 +1,18 @@
+"""
+CP1404/CP5632 Practical
+Refactored version of cumulative total income program
+"""
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
+
+    print_income_report(incomes)
+
+
+main()
