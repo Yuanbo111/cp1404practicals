dec2bc1e91ce6bb0d52cabc7ecfb7aaf957dc9ee
22767 <2276733864@qq.com>
2025-06-20 15:52:52 +0800

    start the hex_colours.py by adding the HEX_COLOURS menu

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,16 @@
+"""
+Lookup hexadecimal colour codes by name.
+"""
+
+HEX_COLOURS = {"Beaver":"#9f170",
+               "Beige":"#f55dc",
+               "Bistre":"#3d2b1f",
+               "BlueViolet":"#8a2be2",
+               "Brass":"#b5a642",
+               "Brick Red":"#cb4154",
+               "Bronze":"#cd7f32",
+               "Brown":"#a52a2a",
+               "Buff":"#f0dc82",
+               "Burgundy":"#8000200"
+}
+
