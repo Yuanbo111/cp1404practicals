f5e111f2f78796ab11989ae6cd6cf22b23714c89
22767 <2276733864@qq.com>
2025-06-20 16:02:02 +0800

    finish the hex_colours.py exercise

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -14,3 +14,12 @@ HEX_COLOURS = {"Beaver":"#9f170",
                "Burgundy":"#8000200"
 }
 
+colour_name = input("Enter colour name (blank to quit): ").strip()
+while colour_name != "":
+    colour_key = colour_name.title()
+    try:
+        hex_code = HEX_COLOURS[colour_key]
+        print(f"{colour_name} is {hex_code}")
+    except KeyError:
+        print("Invalid colour name")
+    colour_name = input("Enter colour name (blank to quit): ").strip()
