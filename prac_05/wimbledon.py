96dfc085b47921d12a5fd630f7910ef35062f333
22767 <2276733864@qq.com>
2025-06-21 12:19:00 +0800

    write the read_data function for wimbledon.py

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -6,8 +6,14 @@ Actual: 20 minutes
 FILENAME = "wimbledon.csv"
 
 def main():
+    """ Read Wimbledon data, count champion wins, collect countries, and display results"""
     data = read_data(FILENAME)
     champion_to_wins = count_champions(data)
     countries = get_champion_countries(data)
     display_results(champion_to_wins, countries)
+
+def read_data(filename):
+    """Read the data from the given CSV file and return a list of rows."""
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        return [line.strip().split(",") for line in in_file.readlines()[1:]]
 
