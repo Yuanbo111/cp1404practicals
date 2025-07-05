a731ad3ce1ecda0f60a78fad9be9604830a2b517
22767 <2276733864@qq.com>
2025-07-06 01:55:06 +0800

    change the import function to avoid ModuleNotFoundError

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -4,7 +4,8 @@ Note that the import has a folder (module) in it.
 This is why we name our folders with no spaces or capitals, as valid module names.
 """
 
-from prac_06.car import Car
+from car import Car
+""" both files are in the same folder, just change the import to from car import Car"""
 
 def main():
     """Demo test code to show how to use car class."""
