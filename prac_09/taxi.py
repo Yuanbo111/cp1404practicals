f89be245b7eebe4f61cc48848eed26643ff5eaa3
22767 <2276733864@qq.com>
2025-07-27 19:15:14 +0800

    adjust the code for taxi.py

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -7,11 +7,11 @@ from car import Car
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
+    price_per_km = 1.23
 
     def __init__(self, name, fuel, price_per_km):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
@@ -20,7 +20,8 @@ class Taxi(Car):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        fare = self.price_per_km * self.current_fare_distance
+        return round(fare, 1)
 
     def start_fare(self):
         """Begin a new fare."""
