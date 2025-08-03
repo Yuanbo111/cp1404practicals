ba7b869aefb6ab6a208e4617962f47a7be429cc5
22767 <2276733864@qq.com>
2025-08-03 16:00:55 +0800

    complete TODO 1234

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -40,15 +40,20 @@ def run_tests():
     # this should pass (no output)
     car = Car()
     assert car._odometer == 0, "Car does not set odometer correctly"
+    assert car.fuel == 0, "Default fuel should be 0"
 
     # TODO: 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
     car = Car(fuel=10)
+    assert car.fuel == 10, "Fuel not set correctly with given value"
+
 
 
 run_tests()
+
+doctest.testmod()
 
 # TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
